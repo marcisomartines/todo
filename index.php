3b681a11dcb0fe119753d394f2675698afcae0f5
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2014-08-28 18:57:31 -0400

    fourth

--- index.php
+++ index.php
@@ -6,8 +6,19 @@
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 	<link href="http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
 	<link rel="stylesheet" href="css/main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-	
+<div class="list">
+	<h1 class="header">To do.</h1>
+	<ul>
+		<li><span class="item">Fazer compras</span></li>
+		<li><span class="item done">Learn php</span></li>
+	</ul>
+
+	<form class="item-add" action="add.php" method="post">
+		<input type="text" name="name" placehold="Digite um novo item aqui." class="input" autocomplete="off" required />
+		<input type="submit" value="Adicionar" class="submit">
+	</form>
 </body>
 </html>
