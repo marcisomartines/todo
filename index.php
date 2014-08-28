b8e18cb2c1f6cc8ca9b0c784b4431d648308db1b
Marciso Gonzalez <marciso.gonzalez@gmail.com>
2014-08-28 18:31:02 -0400

    third

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 <html lang="en">
 <head>
 	<meta charst="UTF-8">
-	<title>Document</title>
+	<title>To do Lista</title>
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 	
